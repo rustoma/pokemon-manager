3337a1f9631bd9c6a06aa8a1b395543afed9e25c
rustoma <t.rusin@qarbon.it>
2025-11-13 21:10:51 +0100

    feat: update loading component to display a centered spinning loader instead of text

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,7 @@
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return <p>Loading...</p>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
 }
