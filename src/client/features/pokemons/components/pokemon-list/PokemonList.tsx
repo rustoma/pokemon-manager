c27da8ecffc2697e711f79a80bf0984a7bf81b40
rustoma <t.rusin@qarbon.it>
2025-11-14 06:22:57 +0100

    feat: display message when no pokemons are found in the PokemonList component

--- src/client/features/pokemons/components/pokemon-list/PokemonList.tsx
+++ src/client/features/pokemons/components/pokemon-list/PokemonList.tsx
@@ -8,6 +8,7 @@ interface PokemonListProps {
 export const PokemonList = ({ pokemons }: PokemonListProps) => {
   return (
     <div className="flex flex-wrap gap-x-4 gap-y-8 py-10">
+      {pokemons.length === 0 && <div className="text-sm text-gray-500">No pokemons found.</div>}
       {pokemons.map((pokemon) => (
         <PokemonListItem key={pokemon.id} pokemon={pokemon} />
       ))}
